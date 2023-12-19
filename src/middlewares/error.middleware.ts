b4c0e8827ba0985bfd39eccd96102b0cd40d1db6
ABHA1 <abha1@ferring.com>
2023-12-19 22:19:03 +0100

    fixed errorhandling  to support openapi validator

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -4,14 +4,14 @@ import { API_ERROR } from "../lib/api.error";
 export function errorMiddleware(app: Application) {
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     if (err instanceof API_ERROR) {
-      return res.status(err.statusCode || 500).json({
+      res.status(err.statusCode || 500).json({
         message: err.message,
         code: err.code,
       });
     } else {
-      res.status(500).json({
+      res.status(err.status || 500).json({
         message: err.message,
-        code: "internal_server_error",
+        errors: err.errors,
       });
     }
   });
