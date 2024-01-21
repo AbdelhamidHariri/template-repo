45b5b541da5a78fabd248b20ea7acfa40061e26a
ABHA1 <abha1@ferring.com>
2024-01-21 21:26:25 +0100

    added mongodb

---
+++ src/lib/mongo.db.ts
@@ -0,0 +1,8 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+export const client = new MongoClient(process.env.MONGODB_URI!, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
